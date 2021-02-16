47303f20056d1c1efd07461e90adb440505260a2
Mahanth <mohan.mahanth@gmail.com>
2021-02-16 13:29:40 -0800

    fixed build script

--- build/build.sh
+++ build/build.sh
@@ -7,11 +7,11 @@ echo "-- Finished MacOS/Darwin build --"
 echo "-- (Step 2) Building Linux binaries --"
 gobin=$PWD/bin/linux/amd64 goos=linux goarch=amd64 go install *.go
 gobin=$PWD/bin/linux/arm64 goos=linux goarch=arm64 go install *.go
-echo "-- Finished MacOS/Darwin build --"
+echo "-- Finished Linux build --"
 
 echo "-- (Step 3) Building Windows binaries --"
 gobin=$PWD/bin/windows/amd64 goos=windows goarch=amd64 go install *.go
 gobin=$PWD/bin/windows/arm goos=windows goarch=arm go install *.go
-echo "-- Finished MacOS/Darwin build --"
+echo "-- Finished Windows build --"
 
 echo "** Finished GopherChat build **"
